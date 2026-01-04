9f20cf102a9c2544de525764a6a6ea31789adbef
omooooori <omooooori432@gmail.com>
2026-01-05 00:18:24 +0900

    Add Android/iOS/Web/Desktop platform folders for both apps

---
+++ apps/learning_app/android/app/src/main/kotlin/com/kidsedu/learning_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.kidsedu.learning_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ apps/picture_book_app/android/app/src/main/kotlin/com/kidsedu/picture_book_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.kidsedu.picture_book_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
